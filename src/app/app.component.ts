db4e4f529f1364666a7b924faecba2a09dadd4ca
Maximino Castillo <maxcastgarcia@gmail.com>
2024-10-29 14:50:40 -0600

    Add components to the main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactPageComponent } from "./contact-page/contact-page.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContactPageComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'contact-page';
+  title = 'Contact';
 }
