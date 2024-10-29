b99deb67ff02cc93b81e72dad03f960bd84cd10b
Maximino Castillo <maxcastgarcia@gmail.com>
2024-10-29 16:59:29 -0600

    Add complete form markup

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactFormComponent } from "../contact-form/contact-form.component";
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports: [],
+  imports: [ContactFormComponent],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.scss'
 })
